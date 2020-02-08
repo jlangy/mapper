3e4f1ff0a97df7c858ab1881a9ed09b0cd7d209d
Cassidy Quaite <cassidy@referralsaasquatch.com>
2020-02-08 22:33:42 +0000

    added seed data for maps and updated views/index

---
+++ db/seeds/02_maps.sql
@@ -0,0 +1,3 @@
+INSERT INTO maps (owner_id, title, description, created_at, default_lat, default_long, collaborative, public, active)
+  VALUES (1, 'Victoria Coffee Shops', 'The best coffee shops in Victoria, BC.', now(), 48.4261, - 123.3642, TRUE, TRUE, TRUE);
+
