091d60e3fdee81812f1356a8610358eb48b13c0a
Cassidy Quaite <cassidy@referralsaasquatch.com>
2020-02-10 18:47:49 +0000

    Added mapsRoutes to server, maps_json route to get maps/pins data from database

--- public/scripts/maps.js
+++ public/scripts/maps.js
@@ -1,22 +1,37 @@
-const db = require("./index");
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/"
+//   }).done(data => {
+//     console.log(data);
+//   });
+// });
+
+// need to make a get request to maps_json where we query the db to get maps/pins data to pass
+// into initMap function to render any mapp with the saved pins
+
+var map;
+var marker;
+
+var position = { lat: 48.4261, lng: -123.3642 };
+var pins = [
+  { lat: 48.4278, lng: -123.369 },
+  { lat: 48.4262, lng: -123.3616 }
+];
 
 function initMap() {
   // The map
-  const map_location = { lat: 48.4261, lng: -123.3642 };
-  const map = new google.maps.Map(document.getElementById("map"), {
+  const map_location = position;
+  map = new google.maps.Map(document.getElementById("map"), {
     zoom: 12,
     center: map_location
   });
-  // The pins
-  const pin1_location = { lat: 48.4278, lng: -123.369 };
-  const pin2_location = { lat: 48.4262, lng: -123.3616 };
 
-  let marker1 = new google.maps.Marker({
-    position: pin1_location,
-    map: map
-  });
-  let marker2 = new google.maps.Marker({
-    position: pin2_location,
-    map: map
-  });
+  // The pins
+  for (const pin of pins) {
+    let marker = new google.maps.Marker({
+      position: pin,
+      map: map
+    });
+  }
 }
