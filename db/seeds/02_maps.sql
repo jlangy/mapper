2a5d08ce0c3185eba4f2614dfcf74733a5b58428
Cassidy Quaite <cassidy@referralsaasquatch.com>
2020-02-13 18:26:32 +0000

    Updated styling for browse and map view and added more seed data

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -3,5 +3,5 @@ INSERT INTO maps (owner_id, title, description, created_at, default_lat, default
         (1, 'Victoria Breweries', 'Visit all of the breweries in Victoria, BC.', now(), 48.4261, - 123.3642, TRUE, TRUE, TRUE),
         (1, 'Lighthouse Labs Employers', 'See where LHL alumni are currently working and where you might be able to get a job!', now(), 48.4261, - 123.3642, TRUE, TRUE, TRUE),
         (2, 'This is a private map', 'Only user with id 2 should access this map.', now(), 48.4261, - 123.3642, TRUE, FALSE, TRUE),
-        (2, 'Fun Group Activities', 'Find something fun to do with your friends in Victoria!', now(), 48.4261, - 123.3642, TRUE, TRUE, TRUE);
+        (2, 'Awesome Group Activities', 'Find something fun to do with your friends in Victoria!', now(), 48.4261, - 123.3642, TRUE, TRUE, TRUE);
 
