fe71dafaeea36f0b723163fa544b4d5fb1899d94
Cassidy Quaite <cassidy@referralsaasquatch.com>
2020-02-22 02:01:38 +0000

    Updated README, db structure to align with what we built, and removed google api key from views so map will render in dev mode

--- db/schema/06_categories.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS categories CASCADE;
-
-CREATE TABLE categories (
-  id serial PRIMARY KEY NOT NULL,
-  category varchar(255)
-);
-
--- db/schema/07_map_categories.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS map_categories CASCADE;
-
-CREATE TABLE map_categories (
-  id serial PRIMARY KEY NOT NULL,
-  map_id integer REFERENCES maps(id) NOT NULL,
-  categories_id integer REFERENCES categories(id) NOT NULL
-);
